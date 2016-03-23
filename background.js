f50c92c3d87b5204117a266eb928e344fcb53688
Terry Seidler <terry.seidler@ogd.nl>
2016-03-23 21:12:27 +0100

    Haal Chuck quotes op via API en toon als notificatie

--- background.js
+++ background.js
@@ -1,15 +1,36 @@
 var numberOfChucks = 0;
 var numChuckImages = 6;
+var chuckAPI = 'http://api.icndb.com/jokes/random';
 
 chrome.browserAction.onClicked.addListener(doChuck);
 
 function doChuck() {
+  // eerst, get een random joke via de API
+  var request = new XMLHttpRequest();
+  request.open('GET',chuckAPI, true);
+  request.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var data = JSON.parse(this.response);
+      showNotification(data.value.joke);
+    } else {
+      showNotification("Er ging iets mis :(");
+    }
+  };
+
+  request.onerror = function () {
+    showNotification("Er ging iets mis :(");
+  };
+
+  request.send();
+}
+
+function showNotification(message) {
   var notificationOptions = {
-    'type':       'image',
-    'title':      'Chuck #' + numberOfChucks + 1,
-    'message':    'Chuck Norris BAM',
-    'iconUrl':    'icons/icon128.png',
-    'imageUrl':   'images/' + Math.floor(1 + Math.random() * numChuckImages) + '.jpg',
+    'type':       'basic',
+    'title':      'Chuck #' + (numberOfChucks + 1),
+    'message':    message,
+    'iconUrl':   'images/' + Math.floor(1 + Math.random() * numChuckImages) + '.jpg',
   };
   chrome.notifications.create(String(numberOfChucks++), notificationOptions);
 }
