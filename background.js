ca4b15faacb7043d60be09c9f47290f26ed919e5
Terry Seidler <terry.seidler@ogd.nl>
2016-03-23 21:42:46 +0100

    Spam random Chucks every x seconds

--- background.js
+++ background.js
@@ -1,10 +1,21 @@
-var numberOfChucks = 0;
+var chucking = false;
 var numChuckImages = 6;
 var chuckAPI = 'http://api.icndb.com/jokes/random';
 
-chrome.browserAction.onClicked.addListener(doChuck);
+chrome.browserAction.onClicked.addListener(toggleChuck);
+function toggleChuck() {
+  chucking = !chucking;
+
+  if(chucking) {
+    chrome.browserAction.setBadgeText({text: 'BAM'});
+    doChuck();
+  } else {
+    chrome.browserAction.setBadgeText({text: ''});
+  }
+};
 
 function doChuck() {
+  if(!chucking) { return; }
   // eerst, get een random joke via de API
   var request = new XMLHttpRequest();
   request.open('GET',chuckAPI, true);
@@ -12,25 +23,29 @@ function doChuck() {
     if (this.status >= 200 && this.status < 400) {
       // Success!
       var data = JSON.parse(this.response);
-      showNotification(data.value.joke);
+      showNotification(data.value.joke, data.value.id);
     } else {
-      showNotification("Er ging iets mis :(");
+      showNotification("Er ging iets mis :(", 0);
     }
   };
 
   request.onerror = function () {
-    showNotification("Er ging iets mis :(");
+    showNotification("Er ging iets mis :(", 0);
   };
-
   request.send();
+
+  var volgendeChuck = Math.floor(5 + Math.random() * 5) * 1000;
+  console.log('Volgende chuck in ' + (volgendeChuck / 1000) + 's');
+  setTimeout(doChuck, volgendeChuck);
+
 }
 
-function showNotification(message) {
+function showNotification(message, number) {
   var notificationOptions = {
     'type':       'basic',
-    'title':      'Chuck #' + (numberOfChucks + 1),
-    'message':    message,
+    'title':      'Chuck fact #' + number,
+    'message':    message.replace(/&quot;/g, '"'),
     'iconUrl':   'images/' + Math.floor(1 + Math.random() * numChuckImages) + '.jpg',
   };
-  chrome.notifications.create(String(numberOfChucks++), notificationOptions);
+  chrome.notifications.create(String(number), notificationOptions);
 }
